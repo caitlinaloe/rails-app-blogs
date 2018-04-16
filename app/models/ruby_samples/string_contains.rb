88bb62073ec12577ab4cfad5f94069404b99dfe9
emily <emily>
2018-04-16 05:58:59 -0400

    Added tests back

--- app/models/ruby_samples/string_contains.rb
+++ app/models/ruby_samples/string_contains.rb
@@ -1,5 +1,4 @@
 def string_contains(my_string, word)
-  my_string.downcase.include? word.downcase
   # Check whether or noy word is included in the string.
 
   # Example: string = 'Hello World' word = 'hello'. The result should be true
