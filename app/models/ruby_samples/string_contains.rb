1ad0693a880ce94bc4d0b525e92179a4a8ad0624
Caitlin Aloe <caitlinaloe@gmail.com>
2018-04-23 17:54:48 -0400

    update 2

--- app/models/ruby_samples/string_contains.rb
+++ app/models/ruby_samples/string_contains.rb
@@ -3,5 +3,7 @@ def string_contains(my_string, word)
 
   # Example: string = 'Hello World' word = 'hello'. The result should be true
   # Example string = 'Nice to meet you' word = 'hello'. The result should be false
+  my_string = my_string.downcase
+  word = word.downcase
   my_string.include?(word) == true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,8 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-
-get '/blogs' => 'blogs#index'
-get '/users' => 'users#show'
-
-
-=======
   root to: 'blogs#index'
 
   resources :blogs
   resources :users
->>>>>>> fad222b49a6ba6461a79ef4ca5b3da26c5e3d644
+
 end
