45027cfd8f6f3b6975bf36057e8207cfcebe241a
Caitlin Aloe <caitlinaloe@gmail.com>
2018-05-02 20:16:12 -0400

    wed class updates

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,17 @@
 class User < ApplicationRecord
+  has_many :comments, dependent: :destroy
+
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: true
+  validates :phone, numericality: { less_than_or_equal_to: 999999999 }
+
+
+  def greet
+    puts greet_again
+    return "Hello, my name is #{names}."
+  end
+
+  private def greet_again
+    'hello'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
 
+  resources :comments
   root to: 'blogs#index'
 
   resources :blogs
   resources :users
+  resources :comments
 
   get 'ana_show', to: 'users#show_saenz'
 end
