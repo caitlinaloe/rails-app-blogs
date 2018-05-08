f34f4de2dfa728828ea2118db8cd38a5d43a25f1
Emily Ring <btyy77c@gmail.com>
2018-05-08 08:51:45 -0400

    Added migrations back

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180502235951) do
+ActiveRecord::Schema.define(version: 20180502235953) do
 
   create_table "comments", force: :cascade do |t|
     t.integer "user_id"
