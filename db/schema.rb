5dd24ce9cc146d98e2b977011c44d3fbd0bc55b5
Caitlin Aloe <caitlinaloe@gmail.com>
2018-05-16 19:47:41 -0400

    UPdates for production

--- db/schema.rb
+++ db/schema.rb
@@ -12,18 +12,18 @@
 
 ActiveRecord::Schema.define(version: 20180506183820) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "blogs", force: :cascade do |t|
     t.string "title"
     t.string "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-ActiveRecord::Schema.define(version: 20180502235953) do
-=======
->>>>>>> c9637af33c464a15bb3fdc80e9054805d875d81d
 
   create_table "comments", force: :cascade do |t|
-    t.integer "user_id"
+    t.bigint "user_id"
     t.string "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -38,4 +38,5 @@ ActiveRecord::Schema.define(version: 20180502235953) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "comments", "users"
 end
